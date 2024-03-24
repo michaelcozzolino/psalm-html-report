b8ecd40d7bf83d09078346a5aa3f991a1823cb4f
M.Cozzolino <michaelcozzolino@outlook.it>
2024-03-24 16:32:00 +0100

    Add tests

---
+++ tests/PluginTest.php
@@ -0,0 +1,14 @@
+<?php declare(strict_types=1);
+
+namespace MichaelCozzolino\PsalmHtmlReport\Tests;
+
+use MichaelCozzolino\PsalmHtmlReport\Plugin;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @covers \MichaelCozzolino\PsalmHtmlReport\Plugin
+ */
+class PluginTest extends TestCase
+{
+
+}
